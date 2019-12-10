21dfa6908e76d9579cc9c258d4be75a9b20bc09a
Lonny Kapelushnik <lonny@lonnylot.com>
2019-12-10 10:37:48 +0000

    Version 1.1.1: Added version config

--- .bump-version.php
+++ .bump-version.php
@@ -4,8 +4,8 @@ return [
     'master_branch' => 'master',
     'develop_branch' => 'develop',
     'version_file' => __DIR__ . '/VERSION',
-    // 'define_version_file' => __DIR__ . '/app/version.php', // you can add a 'PHP accessible' define with the current version
-    // 'define_version_name' => 'APP_VERSION', // the define name
-    // 'changelog_file' => __DIR__ . '/CHANGELOG', // A basic CHANGELOG
-    'release_branches' => ['hotfix' => 'hotfix-*', 'release' => 'release-*'], // The branches you should release on
+    'define_version_file' => __DIR__ . '/src/version.php', // you can add a 'PHP accessible' define with the current version
+    'define_version_name' => 'CRUD_SUGAR_VERSION', // the define name
+    'changelog_file' => __DIR__ . '/CHANGELOG', // A basic CHANGELOG
+    'release_branches' => ['hotfix' => 'hotfix-*', 'release' => 'release-*', 'develop' => 'develop'], // The branches you should release on
 ];
